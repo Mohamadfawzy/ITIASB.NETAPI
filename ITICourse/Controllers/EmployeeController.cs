d1ff9410bd1d246a44f6207c2f372002f6d9be8d
mohamedfawzy <mohamedfawzy733@yahoo.com>
2022-08-07 18:09:45 +0200

    first commit

---
+++ ITICourse/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITICourse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployeeName()
+        {
+            return Ok("Mohamed");
+        }
+    }
+}
